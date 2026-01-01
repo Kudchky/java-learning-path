fbd1950c44e7474fbb25503a4564cabab393641e
Pol Castillo <rpcastilloy@gmail.com>
2025-12-31 20:59:48 -0500

    feat: add string charAt exercise

---
+++ 01-fundamentals/src/com/polcastillo/fundamentals/strings/StringCharAtExercise.java
@@ -0,0 +1,9 @@
+package com.polcastillo.fundamentals.strings;
+
+public class StringCharAtExercise {
+    void main() {
+        String word = "Programar";
+
+        IO.println(word.charAt(0) + " " + word.charAt(word.length() - 1));
+    }
+}
