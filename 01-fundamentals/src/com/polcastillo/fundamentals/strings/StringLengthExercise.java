57b9fe04460afc0e44386f14da482d0983ed547b
Pol Castillo <rpcastilloy@gmail.com>
2025-12-31 20:42:55 -0500

    feat: add string length exercise

---
+++ 01-fundamentals/src/com/polcastillo/fundamentals/strings/StringLengthExercise.java
@@ -0,0 +1,9 @@
+package com.polcastillo.fundamentals.strings;
+
+public class StringLengthExercise {
+    static void main() {
+        String name = "Carlos";
+
+        IO.println("El nombre " + name + " tiene " + name.length() + " caracteres.");
+    }
+}
