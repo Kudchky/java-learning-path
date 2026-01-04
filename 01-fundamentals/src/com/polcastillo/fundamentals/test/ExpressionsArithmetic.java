a01afc2be86e099f0b5cec91949d954b0fb79855
Pol Castillo <rpcastilloy@gmail.com>
2026-01-03 23:37:11 -0500

    feat: add test to expressions arithmetic in class ExpressionsArithmetic

---
+++ 01-fundamentals/src/com/polcastillo/fundamentals/test/ExpressionsArithmetic.java
@@ -0,0 +1,11 @@
+package com.polcastillo.fundamentals.test;
+
+public class ExpressionsArithmetic {
+    void main() {
+        int x = 10;
+        x = x++ + 5;
+
+
+        System.out.println(x);
+    }
+}
