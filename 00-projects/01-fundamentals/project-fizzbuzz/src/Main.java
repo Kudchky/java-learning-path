7fb0cb220c26d43072953ebe05b725f58ad8a88a
Pol Castillo <rpcastilloy@gmail.com>
2026-01-19 23:22:11 -0500

    feat: add project FIZZBUZZ, version final

--- 00-projects/01-fundamentals/project-fizzbuzz/src/Main.java
+++ 00-projects/01-fundamentals/project-fizzbuzz/src/Main.java
@@ -1,21 +1,16 @@
 void main() {
     Scanner sc = new Scanner(System.in);
-    int number;
 
     IO.print("Number: ");
-    number = sc.nextInt();
+    int number = sc.nextInt();
 
-        if (number % 3 == 0 && number % 5 == 0) {
+        if (number % 3 == 0 && number % 5 == 0)
             IO.println("FIZZBUZZ");
-        }
-        else if (number % 5 == 0) {
+        else if (number % 5 == 0)
             IO.println("FIZZ");
-        }
-        else if(number % 3 == 0) {
+        else if(number % 3 == 0)
             IO.println("BUZZ");
-        }
-        else {
+        else
             IO.println(number);
-        }
 }
 
